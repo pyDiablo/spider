b490ce66305a8650bad49ef1f26ecca190fe2db1
Hussein Esmail <hussein.esmail7@gmail.com>
2021-03-27 14:52:12 -0400

    Added compatibility for multiple URL files

--- README.md
+++ README.md
@@ -15,7 +15,13 @@ To get this repository, download it from `Code > Download Zip` or clone it using
 After that is done, change to the `spider/` directory and run `pip install requirements.txt` to make sure you have all the required Python libraries.
 
 ## Adding URLs
-In the `spider.py` file, add the URLs you want scanned to the `scan_queue` array, or add them in a file called `urls.txt` in the same folder.
+### Method 1: Editing spider.py file
+
+
+In the `spider.py` file, add the URLs you want scanned to the `scan_queue` array. Make sure each entry is separated by a comma, and are all in quotation marks.
+
+### Method 2: Make your own .txt file
+`urls.txt` will automatically be recognized if it is created in the same folder, but you can also add multiple file names in the `url_files_input` variable in `spider.py`. You might want to do this if you want to separate link types. Each file supports comments starting with `#`. When typing URLs, you can only have 1 URL per line, and it must be at the beginning of the line.
 
 # Running the Program
 To run this program, open a Terminal in the directory these files are stored, then run `python3 spider.py`.
--- spider.py
+++ spider.py
@@ -2,7 +2,7 @@ from indexer import Indexer
 import os
 
 def main():
-    url_file_input = "urls.txt"
+    url_files_input = ["urls.txt"]
     url_file_output = "all_urls.txt"
 
     idx = Indexer()
@@ -16,11 +16,17 @@ def main():
         'https://mirror.futureweb.be/manjaro/arm-stable/'
     ]
 
-    # If the input URLs file exists, add it to the list as well
-    if url_file_input in os.listdir():
-        with open(url_file_input, 'r') as f:
-            for line in f.readlines():
-                scan_queue.append(line)
+    # If sny of the input URL files exist, add it to the list as well
+    for url_file_input in url_files_input:
+        if url_file_input in os.listdir():
+            with open(url_file_input, 'r') as f:
+                for line in f.readlines():
+                    # Strips whitespace in case there is extra at the end of the line
+                    # Splits by space in case there are multiple words or a comment after that (meaning 1 URL per line and it must be at the beginning of the line)
+                    # If the first character of the first word of the line is '#', skip the line
+                    split = line.strip().split(' ')
+                    if split[0][0] != "#":
+                        scan_queue.append(split[0])
     
     urls = idx.scan(scan_queue)
 
